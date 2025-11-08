2346220a0dcc996e8477d32d62ad86e90a112e89
Nikolay Borisenko <22616990+nvborisenko@users.noreply.github.com>
2025-11-08 19:22:57 +0300

    Shared logging (issue)

--- test/ReportPortal.XUnitReporter.IntegrationTests/UnitTest1.cs
+++ test/ReportPortal.XUnitReporter.IntegrationTests/UnitTest1.cs
@@ -2,10 +2,21 @@ namespace ReportPortal.XUnitReporter.IntegrationTests;
 
 public class UnitTest1
 {
+    private ITestOutputHelper _testOutputHelper;
+
+    public UnitTest1(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper.WithReportPortal();
+    }
+
     [Fact]
     public void Test1()
     {
         Assert.True(true);
+
+        Shared.Context.Current.Log.Info("This is a log message from ReportPortal context.");
+
+        _testOutputHelper.WriteLine("This is a test log message.");
     }
 
     [Fact]
